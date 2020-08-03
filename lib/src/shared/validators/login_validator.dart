c24207059ca90bd8a3dea9bbbd94a86844fd1094
LeoneHerbert <herbleopin@gmail.com>
2020-08-03 19:19:08 -0300

    atualização dos validators

--- lib/src/shared/validators/login_validator.dart
+++ lib/src/shared/validators/login_validator.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 class LoginValidator {
   final emailValidation = StreamTransformer<String, String>.fromHandlers(
     handleData: (usuario, sink){
-      if(usuario.trim().length > 12){
+      if(usuario.trim().length > 13 && usuario.trim().contains("@") && usuario.trim().contains("gmail.com") || usuario.trim().contains("hotmail.com") || usuario.trim().contains("ifma.edu.br") || usuario.trim().contains("outlook.com")){
         sink.add(usuario);
       }else{
         sink.addError("Digite um e-mail válido!");
